a4ad1fd9cc50212a0e8ef94d5a6a9ca450507bcc
kumar91gopi <kumar91gopi@gmail.com>
2015-08-31 03:18:25 +0000

    fixed errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'rpvv-yearbook-kumar91gopi-1.c9.io', port: 8080 }
+  config.action_mailer.default_url_options = { host: '0.0.0.0', port: 8080 }
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
